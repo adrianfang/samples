881c2e8f98ceb967ab2ce422f37554117eef942c
Fang <100039430+adrianfang@users.noreply.github.com>
2026-09-17 16:32:10 -0400

    Update mywebsite.js

--- mywebsite.js
+++ mywebsite.js
@@ -1,6 +1,3 @@
 const newHTML = `<iframe src="https://adrianfang.github.io/" width="100%" height="100%" style="border:none;">
-  <p>Your browser does not support iframes. <a href="https://www.example.com">Click here to visit the site directly.</a></p>
 </iframe>`;
-document.open("text/html");
-document.write(newHTML);
-document.close();
+document.body.innerHTML = newHTML;
